0195680cd927cc7eeb8e8c12c16b9683400d503d
RODRA <ardorinpeace@gmail.com>
2023-12-10 12:47:44 +0300

    Добавьте файлы проекта.

---
+++ bookStore.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <Windows.h>
+#include <fstream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+using std::ofstream;
+using std::ifstream;
+using std::endl;
+
+int main() {
+
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+}
